60230303116f514b0e17f18ba41618076e599993
Josue G <josue.ggh@gmail.com>
2020-05-18 21:05:50 -0500

    Now is paper, rock, scissors, lizzard, spock

--- game.js
+++ game.js
@@ -1,17 +1,19 @@
 ( ()=>{
   console.log('This is the game');
 
-  let buttons = document.querySelectorAll(".button");
-  let user    = document.querySelector("#selections .user");
-  let computer= document.querySelector("#selections .computer");
+  let buttons   = document.querySelectorAll(".button");
+  let user      = document.querySelector("#selections .user");
+  let computer  = document.querySelector("#selections .computer");
+  let gameboard = document.querySelector("#game-board");
 
-  let game_options = ['paper','rock', 'scissors'];
   let score = { won : 0, tie : 0 , lose: 0 };
 
   const game_logic = {
-    'paper': ['rock'],
-    'rock' : ['scissors'],
-    'scissors': ['paper']
+    'paper': ['rock','spock'],
+    'rock' : ['scissors','lizzard'],
+    'scissors': ['paper','lizzard'],
+    'lizzard' : ['spock','paper'],
+    'spock': ['scissors','rock'],
   };
 
   const whoWon = function( player1, player2){
@@ -22,6 +24,7 @@
   }
 
   const computerOption = function(){
+    let game_options = Object.keys(game_logic);
     let computer_selection = game_options[Math.floor(Math.random() * game_options.length)];
     computer.innerHTML = computer_selection;
     return computer_selection;
@@ -39,8 +42,19 @@
     scoreAccumulate ( whoWon(user_selection, computer_selection) );
   };
 
-  for (let index = 0; index < buttons.length; index++){
-    buttons[index].addEventListener( "click", selectOption);
+  const initGame = function(){
+    console.log("Starting game");
+    let game_options = Object.keys(game_logic);
+
+    for(let index = 0; index < game_options.length; index ++){
+      let button = document.createElement("button");
+      button.classList.add("button");
+      button.textContent = game_options[index];
+      button.setAttribute("data-type", game_options[index]);
+      button.addEventListener("click", selectOption);
+      gameboard.appendChild(button);
+    }
   }
 
+  initGame();
 } )( );
